efe171578a5bc7da384428e885f2c5dadb20618d
Abhi Singh <abhi.s@browserstack.com>
2023-05-15 23:28:22 +0530

    changes title

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>StackDemo</title>
+        <title>StackDemo Change</title>
         <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml"/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
