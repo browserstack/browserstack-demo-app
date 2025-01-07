8d0bd5bf209c51cd1cabb8d22e8e0954f3afc071
rohit sahu <93916322+rohitsahu-bstack@users.noreply.github.com>
2025-01-07 18:38:40 +0530

    Revert removing the __next id (#41)
    * added violations for 12 rules
    
    * revert removing next keyword

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,6 @@ import Root from "../src/Root";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    document.querySelector("#__next")?.setAttribute?.("id", "");
-  }, []);
   return (
     <Root>
       <Component {...pageProps} />
