504ddcc0eaf39ac45e5487eccfb5023b895ef0e8
Rohit Sahu <rohit.sa@browserstack.com>
2026-01-24 15:44:17 +0530

    formating fix

--- src/components/Shelf/ShelfHeader/index.jsx
+++ src/components/Shelf/ShelfHeader/index.jsx
@@ -5,20 +5,23 @@ import Sort from '../Sort';
 import Clearfix from '../../Clearfix';
 
 const ShelfHeader = props => {
-  return (<>
-    <div style={{
-      fontSize: '1.5em',
-      fontWeight: 'bold',
-      margin: '0.67em 0'
-    }}>Products</div >
-    <div className="shelf-container-header" style={{ marginRight: '50px' }}>
-      <small className="products-found">
-        <h3>{props.productsLength} Product(s) found.</h3>
-      </small>
-      <Sort />
-      <Clearfix />
-    </div>
-  </>
+  return (
+    <>
+      <div style={{
+        fontSize: '1.5em',
+        fontWeight: 'bold',
+        margin: '0.67em 0'
+      }}>
+        Products
+      </div >
+      <div className="shelf-container-header" style={{ marginRight: '50px' }}>
+        <small className="products-found">
+          <h3>{props.productsLength} Product(s) found.</h3>
+        </small>
+        <Sort />
+        <Clearfix />
+      </div>
+    </>
   );
 };
 
